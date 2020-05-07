9fb457a4e2bdfcfa44c1eb584e55954013082911
fellow <fellong0307@gmail.com>
2020-05-07 16:50:52 +0800

    test pwm signal output

--- Blinky/Blinky.srcs/sources_1/new/blinky.vhdl
+++ Blinky/Blinky.srcs/sources_1/new/blinky.vhdl
@@ -33,35 +33,87 @@ use IEEE.numeric_std.all;
 --use UNISIM.VComponents.all;
 
 entity blinky is
-    Port ( GCLK : in STD_LOGIC;
-           LD0 : out STD_LOGIC);
+    Port ( 
+            GCLK : in STD_LOGIC;
+            SW0 : in STD_LOGIC;
+            SW1 : in STD_LOGIC;
+            SW2 : in STD_LOGIC;
+            SW3 : in STD_LOGIC;
+            SW4 : in STD_LOGIC;
+            SW5 : in STD_LOGIC;
+            SW6 : in STD_LOGIC;
+            SW7 : in STD_LOGIC;
+            LD0 : out STD_LOGIC;
+            LD1 : out STD_LOGIC;
+            LD2 : out STD_LOGIC;
+            LD3 : out STD_LOGIC;
+            LD4 : out STD_LOGIC;
+            LD5 : out STD_LOGIC;
+            LD6 : out STD_LOGIC;
+            LD7 : out STD_LOGIC
+            );
 end blinky;
 
 architecture Behavioral of blinky is
-    constant max_count : natural := 1000000; --1000000=5000us
     signal Rst : std_logic;
  begin
-
-    Rst <= '0';
  
-    -- 0 to max_count counter
-    compteur : process(GCLK, Rst)
-        variable count : natural range 0 to max_count;
+    Rst <= '0';
+    
+    func_pwm : process(GCLK, Rst)
+    
+        constant pwm0_period : natural := 1000000; --1000000=5000us
+        variable pwm0_duty : natural range 0 to pwm0_period := 0;
+        variable pwm0_count : natural range 0 to pwm0_period := 0;
+        variable dir : natural range 0 to 1 := 0;   
+             
+        constant max_delay_count : natural := 100; --1000000=5000us
+        variable delay_count : natural range 0 to max_delay_count := 0;
+
     begin
+    
         if Rst = '1' then
-            count := 0;
-            LD0 <= '1';
-        elsif rising_edge(GCLK) then
-            if count < max_count/2 then
-                count := count + 1;
-                LD0 <= '1';
-            elsif count < max_count then
-                LD0 <= '0';
-                count := count + 1;
-            else
-                LD0 <= '1';
-                count := 0;
-            end if;
+            pwm0_duty := 0;
         end if;
-    end process compteur; 
+        
+        if rising_edge(GCLK) then
+            if pwm0_count < pwm0_duty then
+                LD0 <= '1';
+            else
+                LD0 <= '0';
+            end if;
+            
+            pwm0_count := pwm0_count + 1;
+            if pwm0_count = pwm0_period then
+                pwm0_count := 0;
+            end if;
+            
+            delay_count := delay_count + 1;
+            if delay_count = max_delay_count then
+                delay_count := 0;
+                if dir  = 0 then
+                    pwm0_duty := pwm0_duty + 1;
+                    if pwm0_duty = pwm0_period then
+                        dir := 1;
+                    end if;
+                else
+                    pwm0_duty := pwm0_duty - 1;
+                    if pwm0_duty = 0 then
+                        dir := 0;
+                    end if;
+                end if;
+            end if;
+            
+        end if;
+    end process func_pwm; 
+    
+    sw1_led1 : process(GCLK)
+    begin
+        if SW1 = '1' then
+            LD1 <= '1';
+        else
+            LD1 <= '0';
+        end if;
+    end process sw1_led1; 
+    
 end Behavioral;
